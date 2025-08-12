3589bf9b4563ad70379f807df08333c2241abbfb
SPraveeen <praveenji8498@gmail.com>
2025-08-12 21:27:45 +0530

    logging

---
+++ logging/helper.py
@@ -0,0 +1,18 @@
+# import logging
+# logging.debug('This is a debug message')
+# logging.info('this is an info message')
+# logging.warning("This is a warning message")
+# logging.error("This is an error message")
+# logging.critical("This is a critical message")
+
+# import logging
+# logging.basicConfig(level=logging.DEBUG,format='%(asctime)s-%(name)s-%(levelname)s-%(message)s',datefmt='%m/%d/%Y %H:%M:%S')
+# logging.debug('This is a debug message')
+# logging.info('this is an info message')
+# logging.warning("This is a warning message")
+# logging.error("This is an error message")
+# logging.critical("This is a critical message")
+
+import logging
+logging.basicConfig(level=logging.DEBUG,format='%(asctime)s-%(name)s-%(levelname)s-%(message)s',datefmt='%m/%d/%Y %H:%M:%S')
+import main
